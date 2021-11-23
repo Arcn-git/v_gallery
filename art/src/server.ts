cb827f6de8449e666a89bdabf48f6ebe24747040
Arcn <arcn.storm@web.de>
2021-11-23 23:49:52 +0100

    Some setup of art and removed react image

--- art/src/server.ts
+++ art/src/server.ts
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+//Middleware for ArtService
+app.use(express.json());
+
+// Port !TODO! Change to env
 const PORT = 7001;
 
 app.get('/', (req:any, res:any) => {
